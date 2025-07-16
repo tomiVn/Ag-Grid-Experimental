e9a746dad41e16a25649e7467ac39ae6ff3d2e09
Tomi Nikolov <tomi.v.nikolov@gmail.com>
2025-07-16 07:47:52 +0300

    fetch data, add filters and reset

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef, GridReadyEvent, GridApi } from 'ag-grid-community';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    imports: [AgGridAngular],
+    templateUrl: './app.html',
+    styleUrl: './app.css'
 })
 export class App {
-  protected title = 'ag-grid-v31';
+
+    http    = inject(HttpClient);
+    rowData = signal<any[]>([]);
+    data    = signal<any[]>([]);;
+    
+    gridApi!: GridApi;
+
+    colDefs: ColDef[] = [
+        { field: "mission"    },
+        { field: "company"    },
+        { field: "location"   },
+        { field: "date"       },
+        { field: "price"      },
+        { field: "successful" },
+        { field: "rocket"     }
+    ];
+
+    constructor(){
+
+        this.colDefs = this.colDefs.map((obj: any) => 
+            ({ ...obj, filter: true, filterParams: { applyMiniFilterWhileTyping: true } }))
+    }
+
+    onGridReady(params: GridReadyEvent) {
+
+        this.http
+            .get<any[]>('https://www.ag-grid.com/example-assets/space-mission-data.json')
+            .subscribe(data => {
+                this.gridApi = params.api;
+                this.rowData.set(data);
+                this.data.set(data);
+            });
+    }
+
+    resetFilterValues(){
+
+        this.gridApi.setFilterModel(null);
+        this.gridApi.applyColumnState( { defaultState: { sort: null }} );
+        this.gridApi.onFilterChanged(); 
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+import { AllEnterpriseModule } from 'ag-grid-enterprise';
+
+ModuleRegistry.registerModules([AllCommunityModule, AllEnterpriseModule]);
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
