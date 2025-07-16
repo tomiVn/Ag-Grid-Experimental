07a4ea3432b8106954cc7279d3a64c8ccef9c7cd
Tomi Nikolov <tomi.v.nikolov@gmail.com>
2025-07-16 14:02:29 +0300

    add 2 tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,13 +1,32 @@
 import { provideZonelessChangeDetection } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import { TestBed, tick } from '@angular/core/testing';
 import { App } from './app';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { ModuleRegistry, AllCommunityModule, type GridReadyEvent } from 'ag-grid-community';
+import { CommonModule } from '@angular/common';
 
 describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+
+  let httpTesting: HttpTestingController;
+
+  ModuleRegistry.registerModules([AllCommunityModule]);
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideZonelessChangeDetection()]
-    }).compileComponents();
+      providers: [
+        provideZonelessChangeDetection(), // ✅ required in Angular 20
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]
+    });
+
+    httpTesting = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTesting.verify();
   });
 
   it('should create the app', () => {
@@ -16,10 +35,26 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
+  it('load data', () => {
     const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ag-grid-v31');
+    const component = fixture.componentInstance;
+
+    const mockApi = { setFilterModel: jasmine.createSpy() } as any;
+    const event = { api: mockApi } as GridReadyEvent;
+
+    component.onGridReady(event); 
+
+    const req = httpTesting.expectOne('https://www.ag-grid.com/example-assets/space-mission-data.json');
+    
+    expect(req.request.method).toBe('GET');
+
+    const mockData = [
+      { mission: 'Apollo 11', company: 'NASA' },
+      { mission: 'Falcon 9', company: 'SpaceX' },
+    ];
+
+    req.flush(mockData);
+
+    expect(component.data()).toEqual(mockData);
   });
 });
